747fc3a9a2c04cc41449e13133e910fecac9a8c5
4rn4u <vilx00@protonmail.com>
2024-03-17 17:52:42 +0100

    changes

--- index.js
+++ index.js
@@ -9,31 +9,22 @@ const createWindow = () => {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
-    }
+    },
+    
   })
 
   win.loadFile('index.html')
+  win.setMenu(null)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Algunas APIs pueden solamente ser usadas despues de que este evento ocurra.
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (!isMac) app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. Tu también puedes ponerlos en archivos separados y requerirlos aquí.
