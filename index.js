c4c175050644f9de1aa947a55fb4dbf27d78369f
4rn4u <vilx00@protonmail.com>
2024-03-17 19:12:46 +0100

    changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const createWindow = () => {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     },
-    
+    icon: path.join(__dirname, './assets/icons/setting.svg')
   })
 
   win.loadFile('index.html')
